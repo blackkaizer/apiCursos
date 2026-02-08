86c32822ba803014b9505955ae66832efbd55b42
Eliton Fogaca <elitonkfogaca@gmail.com>
2026-02-08 19:58:44 -0300

    fix: fix database configuration and update .env.example

--- app/config.py
+++ app/config.py
@@ -1,9 +1,13 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 class Config:
+    MONGODB_SETTINGS = {
+        "db": os.getenv("MONGO_DB"),
+        "host": os.getenv("MONGO_HOST"),
+        "port": 27017,
+        "username": os.getenv("MONGO_USER"),
+        "password": os.getenv("MONGO_PASS"),
+        "authentication_source": "admin"
+    }
+
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    MONGO_URI = os.getenv("MONGO_URI")
-    MONGO_DB = os.getenv("MONGO_DB")
--- app/extensions/db.py
+++ app/extensions/db.py
@@ -1,8 +1,4 @@
 from mongoengine import connect
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 def init_db(app):
-    connect(db='curses_db', host=os.getenv('MONGO_URI'))
+    connect(**app.config["MONGODB_SETTINGS"])
